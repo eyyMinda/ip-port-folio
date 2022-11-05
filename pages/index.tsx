ed3ce9cf709964aafa9311d63fedc5026206d844
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-05 20:15:02 +0200

    Background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-y-scroll overflow-x-hidden z-0'>
       <Head>
         <title>IP:PORT --folio</title>
       </Head>
 
       <Header />
-      {/* Hero */}
+
+      <section id="Hero" className='snap-center'>
+        <Hero />
+      </section>
 
       {/* About */}
 
