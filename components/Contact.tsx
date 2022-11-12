75971d6d77a979cfab3499ac565bc798a9ff1373
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-12 22:50:38 +0200

    react-hook-form replaced with emailjs

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { EnvelopeIcon } from '@heroicons/react/24/solid';
-import { useForm, SubmitHandler } from 'react-hook-form';
-
-type Inputs = {
-  name: string,
-  email: string,
-  subject: string,
-  message: string
-}
+import emailjs from 'emailjs-com';
 
 type Props = { dark: boolean, screenWidth: number }
 
 export default function Contact({ dark, screenWidth }: Props) {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
 
-  const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+    emailjs.sendForm('service_jshvqpd', 'template_p4hn2il', e.currentTarget, '3oJEuimazmiLWmPDH')
+      .then((result) => {
+        alert('You have succesfully sent an email');
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.currentTarget.reset();
+  };
 
   return <div className='section'>
     <h3 className='sectionHeading'>Contact</h3>
@@ -27,20 +28,20 @@ export default function Contact({ dark, screenWidth }: Props) {
         <p className='text-lg sm:text-2xl'>sublimemindrite@gmail.com</p>
       </div>
 
-      <form onSubmit={handleSubmit(onSubmit)} className={`${dark ? '' : 'light'}
+
+      <form onSubmit={sendEmail} className={`${dark ? '' : 'light'}
          flex flex-col space-y-2 w-[85%] sm:w-fit mx-auto`}>
 
         <div className='flex space-x-2'>
-          <input {...register('name')} placeholder='Name' className='contactInput' type="text" />
-
-          {screenWidth >= 640 ? <input {...register('email')} placeholder='Email'
-            className='contactInput' type="email" /> : ''}
+          <input placeholder='Name' className='contactInput' type="text" name="name" />
+          {screenWidth >= 640 ? <input placeholder='Email'
+            className='contactInput' type="email" name="email" /> : ''}
         </div>
-        {screenWidth < 640 ? <input {...register('email')} placeholder='Email'
-          className='contactInput' type="email" /> : ''}
+        {screenWidth < 640 ? <input placeholder='Email'
+          className='contactInput' type="email" name="email" /> : ''}
 
-        <input {...register('subject')} placeholder='Subject' className='contactInput' type="text" />
-        <textarea {...register('message')} placeholder='Message' className='contactInput' />
+        <input placeholder='Subject' className='contactInput' type="text" name="subject" />
+        <textarea placeholder='Message' className='contactInput' name="message" />
 
         <button type='submit' className={`py-5 px-10 rounded-md font-bold
           ${dark ? 'bg-primary-500' : 'bg-secondary-400'}`}>Submit</button>
