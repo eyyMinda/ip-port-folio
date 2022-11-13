5e661bb1c923090f5ad0946ee3ea3f7c4db66394
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-13 20:18:26 +0200

    Sanity backend

---
+++ sanity/schemas/pageInfo.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'pageInfo',
+  title: 'PageInfo',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
+      name: "bgInformation",
+      title: "BgInformation",
+      type: "string",
+    },
+    {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "socials",
+      title: "Socials",
+      type: "array",
+      of: [{ type: "reference", to: { type: "social" } }],
+    },
+  ],
+};
