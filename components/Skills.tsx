d0dbad30a565d2bb96f9da4321ff9338e6166034
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-15 02:43:11 +0200

    Skills data from back-end

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,14 @@
 import { motion } from 'framer-motion';
 import React from 'react';
+import { Skill as SkillType } from '../typings';
 import Skill from './Skill';
 
-type Props = { dark: boolean }
+type Props = {
+  dark: boolean;
+  skills: SkillType[];
+}
 
-export default function Skills({ dark }: Props) {
+export default function Skills({ dark, skills }: Props) {
   return <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}
     transition={{ duration: 1.5 }}
     className='section max-w-[2000px] xl:px-10 xl:space-y-0 text-center md:text-left'>
@@ -13,18 +17,10 @@ export default function Skills({ dark }: Props) {
       Hover to preview my current skill-progress
     </h3>
 
-    <div className='grid grid-cols-3 sm:grid-cols-4 gap-1'>
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
-      <Skill />
+    <div className='grid grid-cols-3 gap-1 sm:grid-cols-4 sm:gap-5'>
+      {skills?.map((skill, i) => (<Skill key={skill._id} skill={skill}
+      />))}
+      {/* directionLeft */}
     </div>
   </motion.div>
 }
