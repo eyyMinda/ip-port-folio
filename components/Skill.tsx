890470f74c9e3537e3d5de5862e2c2a6a181fbdc
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-15 04:03:57 +0200

    Skill style adjustments

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -15,10 +15,10 @@ export default function Skill({ directionLeft, skill }: Props) {
     <motion.img initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
       whileInView={{ x: 0, opacity: 1 }} transition={{ duration: .3 }}
       src={urlFor(skill.image).url()} alt={skill.title}
-      className='rounded-full border border-gray-500 object-cover h-auto w-20 md:w-28
+      className='rounded-full border border-gray-700 object-cover h-auto w-20 md:w-28
       xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out'
     />
-    <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
+    <div className='absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out
     group-hover:bg-white w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
       <div className='flex items-center justify-center h-full'>
         <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
