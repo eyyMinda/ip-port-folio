2bfd09de3259ab40ec911ee7be9ade5bb780c8ca
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-15 22:39:51 +0200

    Hero, about mobile adjustments

--- components/About.tsx
+++ components/About.tsx
@@ -18,14 +18,14 @@ export default function About({ dark, pageInfo }: Props) {
       <motion.img initial={{ x: -200, opacity: 0 }} transition={{ duration: 1.4 }}
         whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true }}
         src={urlFor(pageInfo?.profilePic).url()}
-        className='flex-shrink-0 hidden xs:block w-32 h-32 sm:w-52 sm:h-52 rounded-full mt-24 sm:mt-0
+        className='flex-shrink-0 hidden xs:block w-32 h-32 sm:w-52 sm:h-52 rounded-full mt-10 sm:mt-0
         object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]' />
 
-      <div className='space-y-4 sm:space-y-10 px-0 md:px-10 drop-shadow-2xl'>
+      <div className='space-y-4 sm:space-y-10 md:px-10 drop-shadow-2xl m-4'>
 
         <h4 className={`${dark ? '' : 'light'} h4`}>Know <span>Thy</span> Author</h4>
 
-        <p className='text-base tracking-wide'>
+        <p className='text-base tracking-wide overflow-y-scroll scrollbar max-h-60 sm:max-h-[100%]'>
           {pageInfo?.bgInformation}
         </p>
       </div>
