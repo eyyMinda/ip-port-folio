f955f7976aac1b359085fda099dccee60964cbc8
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-16 14:23:11 +0200

    ye

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,14 +10,14 @@ export default function Projects({ dark, projects }: Props) {
   return <div className='section'>
     <h3 className='sectionHeading'>Projects</h3>
 
-    <div className='relative w-full flex overflow-x-scroll overflow-y-hidden
-     snap-x snap-mandatory z-10 scrollbar'>
+    <div className={`${dark ? '' : 'light'} relative w-full flex
+     overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-10 scrollbar`}>
       {projects?.map((project, i) => (
         <Project key={project._id} dark={dark} count={[i + 1, projects.length]} project={project} />
       ))}
     </div>
 
     {/* Background Line */}
-    <div className='w-full absolute top-[30%] bg-secondary-300/10 left-0 h-[500px] -skew-y-12'></div>
+    <div className='w-full absolute top-[30%] bg-secondary-500/10 left-0 h-[500px] -skew-y-12'></div>
   </div>
 }
