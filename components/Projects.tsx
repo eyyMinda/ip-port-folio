6ee7f648e0550603d05df5476b05db40c5b235b9
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-17 01:10:03 +0200

    Projects swipe button, not functional

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import { Project as ProjectType } from '../typings';
 import Project from './Project';
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/24/solid';
 
 type Props = {
   dark: boolean;
@@ -7,7 +8,7 @@ type Props = {
 }
 
 export default function Projects({ dark, projects }: Props) {
-  return <div className='section'>
+  return <div className='section relative'>
     <h3 className='sectionHeading'>Projects</h3>
 
     <div className={`${dark ? '' : 'light'} relative w-full flex
@@ -16,6 +17,11 @@ export default function Projects({ dark, projects }: Props) {
         <Project key={project._id} dark={dark} count={[i + 1, projects.length]} project={project} />
       ))}
     </div>
+
+    <div className='left-10 swipeBtn bg-gradient-to-l animate-gradientXreverse'>
+      <ChevronDoubleLeftIcon className='w-7 h-auto hidden md:block' /></div>
+    <div className='right-10 swipeBtn bg-gradient-to-r animate-gradientX'>
+      <ChevronDoubleRightIcon className='w-7 h-auto hidden md:block' /></div>
 
     {/* Background Line */}
     <div className='w-full absolute top-[30%] bg-secondary-500/10 left-0 h-[500px] -skew-y-12'></div>
