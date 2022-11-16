caf5fc2e6820d5a0ad3e98b53def1be8eb604e8e
Mindaugas Straksys <100799754+eyyMinda@users.noreply.github.com>
2022-11-16 17:37:19 +0200

    Skill onHover display skill title

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -20,10 +20,11 @@ export default function Skill({ directionLeft, skill }: Props) {
       className='rounded-full border border-gray-700 object-cover h-auto w-20 md:w-28
       xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out'
     />
-    <div className='absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out
+    <div className='absolute opacity-0 group-hover:opacity-70 transition duration-300 ease-in-out
     group-hover:bg-white w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
-      <div className='flex items-center justify-center h-full'>
-        <p className='text-3xl font-bold text-black opacity-100'>{skill.progress}%</p>
+      <div className='flex flex-col items-center justify-center h-full font-bold text-black'>
+        <p className='text-xl opacity-100'>{skill.title}</p>
+        <p className='text-3xl opacity-100'>{skill.progress}%</p>
       </div>
     </div>
   </div>
