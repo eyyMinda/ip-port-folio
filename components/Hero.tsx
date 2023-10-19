e5e2e7d612f2d2cd5e8ac773404fdf9ef22cf9ec
Mindaugas Straksys <gamerx14x@gmail.com>
2023-10-19 04:58:55 +0300

    removed Midnight from Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,10 +8,9 @@ type Props = {
   dark: boolean;
   pageInfo: PageInfo;
   isExperiences: number;
-  setMidnight: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function Hero({ dark, pageInfo, isExperiences, setMidnight }: Props) {
+export default function Hero({ dark, pageInfo, isExperiences }: Props) {
   const [text, count] = useTypewriter({
     words: [`${pageInfo?.name}`, "Logical-Competitive.jsx", "<YetPlayful&Creative />"],
     loop: true,
@@ -26,8 +25,9 @@ export default function Hero({ dark, pageInfo, isExperiences, setMidnight }: Pro
       <BackgroundCircles dark={dark} />
 
       <div className='z-20'>
-        <h2 className={`${dark ? 'text-gray-500 ' : 'text-gray-800 '}
-         text-sm uppercase tracking-[10px]`}>{pageInfo?.role}</h2>
+        <h2 className={`text-gray-${dark ? '500' : '800'} text-sm uppercase tracking-[10px]`}>
+          {pageInfo?.role}
+        </h2>
 
         <h1 className='text-2xl xs:text-[1.8rem] lg:text-5xl font-semibold py-5
          max-w-[300px] xs:max-w-none mx-auto'>
@@ -35,17 +35,12 @@ export default function Hero({ dark, pageInfo, isExperiences, setMidnight }: Pro
           <Cursor cursorColor={dark ? 'darkred' : 'coral'} />
         </h1>
 
-        <div className={`${dark ? '' : 'light'} pt-5
-        flex flex-wrap justify-center gap-2`}>
+        <div className={`${!dark && 'light'} pt-5 flex flex-wrap justify-center gap-2`}>
           <Link href="#about" className='heroButton'>About</Link>
           {isExperiences ? <Link href="#experience" className='heroButton'>Experience</Link> : ''}
           <Link href="#skills" className='heroButton'>Skills</Link>
           <Link href="#projects" className='heroButton'>Projects</Link>
         </div>
-        <h5 className="font-extrabold text-sm mt-10 underline text-synthpink opacity-[0.05]
-        cursor-pointer hover:opacity-100 transition-all" onClick={() => setMidnight(m => !m)}>
-          Toggle Midnight
-        </h5>
       </div>
 
     </div>
