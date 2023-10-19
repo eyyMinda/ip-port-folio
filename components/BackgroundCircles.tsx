044b5dcb76cc54ad37d25fa9853b1550695e3616
Mindaugas Straksys <gamerx14x@gmail.com>
2023-10-19 19:21:58 +0300

    Update BackgroundCircles.tsx

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -9,11 +9,11 @@ export default function BackgroundCircles({ dark }: Props) {
     <motion.div initial={{ opacity: 0 }}
       animate={{
         scale: [1, 2, 2, 3, 1],
-        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        opacity: [0.1, 0.2, 0.4, 0.6, 0.1, 0.8],
         borderRadius: ["20%", "20%", "50%", "80%", "20%"]
       }}
       transition={{ duration: 2.5 }}
-      className={`relative flex justify-center items-center ${dark ? '' : 'light'}`}>
+      className={`relative flex justify-center items-center ${!dark && 'light'}`}>
       <div className='h-[200px] w-[200px] ping' />
       <div className='h-[300px] w-[300px] ping' />
       <div className='h-[500px] w-[500px] ping' />
