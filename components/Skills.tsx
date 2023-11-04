9bc849975a493d0f11bf8d36fa464b2abb691ddd
Mindaugas Straksys <gamerx14x@gmail.com>
2023-11-04 03:34:14 +0200

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,14 +13,14 @@ export default function Skills({ dark, skills }: Props) {
     transition={{ duration: 1.5 }}
     className='section max-w-[2000px] xl:px-10 text-center md:text-left'>
     <h3 className='sectionHeading'>Skills</h3>
-    <h3 className='sectionHeading normal-case w-[95vw] sm:w-[75vw] lg:w-[60vw] top-36 text-sm tracking-widest'>
-      Percentages reflect my practical experience with each skill rather than a comprehensive mastery of all aspects. Think of them as gauges of my ability to implement these skills effectively in real-world projects.
+    <h3 className='sectionHeading normal-case w-[95vw] sm:w-[75vw] lg:w-[60vw] top-36 text-xs sm:text-sm tracking-widest'>
+      Percentages reflect my practical experience with each skill rather than a comprehensive mastery of all aspects - my ability to implement the skills effectively in real-world projects.
     </h3>
 
     <div className='grid grid-cols-3 gap-1 sm:grid-cols-4 sm:gap-5 mt-32'>
       {skills?.map((skill, i) => (
         <Skill key={skill._id} skill={skill}
-          directionLeft={skills.length / 2 > i + 1 ? true : false} />))}
+          directionLeft={skills.length / 2 > i + 1} />))}
     </div>
   </motion.div>
 }
