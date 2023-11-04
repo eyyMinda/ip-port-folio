581482d0fb921a4a9440056bad8500519c6aa662
eyyMinda <gamerx14x@gmail.com>
2023-11-04 04:37:23 +0200

    updated sanity dep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <html suppressHydrationWarning>
-      <Head>
-        <title>IP:PORT --folio</title>
-      </Head>
-      <body>
-        <Component {...pageProps} />
-        <Analytics />
-      </body>
-    </html>
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 }
