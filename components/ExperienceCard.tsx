746e28b6c82f6b6992641e9c6a874beeaab7f0cf
eyyMinda <gamerx14x@gmail.com>
2023-11-04 04:17:14 +0200

    img to next/image; updated dependencies

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,47 +1,71 @@
-import { useMediaQuery } from 'react-responsive';
-import { motion } from 'framer-motion';
-import { urlFor } from '../sanity';
-import { Experience } from '../typings';
+import { useMediaQuery } from "react-responsive";
+import { motion } from "framer-motion";
+import { urlFor } from "../sanity";
+import { Experience } from "../typings";
+import Image from "next/image";
 
 type Props = {
   exp: Experience;
-}
+};
 
 export default function ExperienceCard({ exp }: Props) {
-  const isBigScreen = useMediaQuery({query:"only screen and (min-width : 460px)"});
-  return <motion.article initial={{ opacity: 0.4 }}
-    whileInView={{ opacity: 1 }} transition={{ duration: .4 }}
-    className='flex flex-col rounded-2xl items-center flex-shrink-0 snap-center
+  const isBigScreen = useMediaQuery({
+    query: "only screen and (min-width : 460px)",
+  });
+  return (
+    <motion.article
+      initial={{ opacity: 0.4 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.4 }}
+      className="flex flex-col rounded-2xl items-center flex-shrink-0 snap-center
     w-full md:w-[600px] xl:w-[900px] space-y-0 sm:space-y-4 bg-gray-700 
-    p-4 sm:p-10 overflow-hidden'>
+    p-4 sm:p-10 overflow-hidden">
+      {isBigScreen && (
+        <motion.img
+          initial={{ y: -100, opacity: 0 }}
+          transition={{ duration: 1.2 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className="w-16 h-16 rounded-full xl:w-[200px] xl:h-[200px]
+      object-cover object-center"
+          src={urlFor(exp.companyImage).url()}
+          alt={exp.company}
+        />
+      )}
 
-    {isBigScreen && <motion.img initial={{ y: -100, opacity: 0 }} transition={{ duration: 1.2 }}
-      whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }}
-      className='w-16 h-16 rounded-full xl:w-[200px] xl:h-[200px]
-      object-cover object-center'
-      src={urlFor(exp.companyImage).url()} alt={exp.company} />}
+      <div className="px-0 md:px-10 text-white">
+        <h4 className="text-4xl font-light">{exp.jobTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{exp.company}</p>
 
-    <div className='px-0 md:px-10 text-white'>
-      <h4 className='text-4xl font-light'>{exp.jobTitle}</h4>
-      <p className='font-bold text-2xl mt-1'>{exp.company}</p>
+        <div className="flex space-x-2 my-2">
+          {exp.technologies.map(tech => (
+            <Image
+              src={urlFor(tech.image).url()}
+              alt={tech.title}
+              width={200}
+              height={200}
+              className="w-8 h-auto mr-1"
+              key={tech._id}
+            />
+          ))}
+        </div>
 
-      <div className='flex space-x-2 my-2'>
-        {exp.technologies.map(tech => (
-          <img className='w-8 h-auto mr-1' key={tech._id}
-            src={urlFor(tech.image).url()} alt={tech.title} />
-        ))}
-      </div>
+        <p className="py-4 text-gray-500">
+          {new Date(exp.dateStarted).toDateString()} -{" "}
+          {exp.currentlyWorking
+            ? "Present"
+            : new Date(exp.dateEnded).toDateString()}
+        </p>
 
-      <p className='py-4 text-gray-500'>
-        {new Date(exp.dateStarted).toDateString()} -{' '}
-        {exp.currentlyWorking ? 'Present' : new Date(exp.dateEnded).toDateString()}
-      </p>
-
-      <ul className='list-disc xs:space-y-2 ml-5 text-lg pr-4
+        <ul
+          className="list-disc xs:space-y-2 ml-5 text-lg pr-4
        max-h-[200px] xs:max-h-[300px] sm:max-h-[100%] overflow-y-scroll
-      scrollbar-thin scrollbar-track-gray-900/20 scrollbar-thumb-gray-500'>
-        {exp.points.map((p, i) => <li key={i}>{p}</li>)}
-      </ul>
-    </div>
-  </motion.article>
+      scrollbar-thin scrollbar-track-gray-900/20 scrollbar-thumb-gray-500">
+          {exp.points.map((p, i) => (
+            <li key={i}>{p}</li>
+          ))}
+        </ul>
+      </div>
+    </motion.article>
+  );
 }
--- components/Project.tsx
+++ components/Project.tsx
@@ -1,39 +1,65 @@
-import { useMediaQuery } from 'react-responsive';
-import { motion } from 'framer-motion';
-import { urlFor } from '../sanity';
-import { Project as ProjectType } from '../typings';
+import { useMediaQuery } from "react-responsive";
+import { motion } from "framer-motion";
+import { urlFor } from "../sanity";
+import { Project as ProjectType } from "../typings";
+import Image from "next/image";
 
 type Props = {
   project: ProjectType;
   dark: boolean;
   count: Array<number>;
-}
+};
 
 export default function Project({ project, count, dark }: Props) {
-  const isBigScreen = useMediaQuery({query:"only screen and (min-width : 460px)"});
-  
-  return <div className='h-screen w-screen flex-shrink-0 snap-center space-y-1 xs:space-y-4
-        flex flex-col items-center justify-center p-5 mt-4 sm:mt-0 sm:p-20 md:p-44'>
-    <a href={project.linkToBuild} target='_blank' rel="noreferrer"
-      className='hover:text-white text-gray-400'>
-      {isBigScreen && <motion.img initial={{ y: -300, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1.2 }} viewport={{ once: true }}
-        src={urlFor(project.image).url()} alt={project.title}
-        className='w-auto h-24 sm:h-44 hidden xs:block' />}
-      <p className='text-center underline'>Link to Build</p>
-    </a>
+  const isBigScreen = useMediaQuery({
+    query: "only screen and (min-width : 460px)",
+  });
 
-    <div className='space-y-10 px-0 md:px-10 max-w-6xl text-center'>
-      <h4 className={`${!dark && 'light'} h4`}>
-        <span>Case Study {count[0]} of {count[1]}:</span>{" "}{project.title}
-      </h4>
-    </div>
+  return (
+    <div
+      className="h-screen w-screen flex-shrink-0 snap-center space-y-1 xs:space-y-4
+        flex flex-col items-center justify-center p-5 mt-4 sm:mt-0 sm:p-20 md:p-44">
+      <a
+        href={project.linkToBuild}
+        target="_blank"
+        rel="noreferrer"
+        className="hover:text-white text-gray-400">
+        {isBigScreen && (
+          <motion.img
+            initial={{ y: -300, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.2 }}
+            viewport={{ once: true }}
+            src={urlFor(project.image).url()}
+            alt={project.title}
+            className="w-auto h-24 sm:h-44 hidden xs:block"
+          />
+        )}
+        <p className="text-center underline">Link to Build</p>
+      </a>
 
-    <p className='text-lg md:text-left'>{project?.summary}</p>
-    <div className='flex justify-center items-center space-x-2'>
-      {project.technologies.map(skill => (
-        <img key={skill._id} className='w-10 h-auto' src={urlFor(skill.image).url()} alt={skill.title} />
-      ))}
+      <div className="space-y-10 px-0 md:px-10 max-w-6xl text-center">
+        <h4 className={`${!dark && "light"} h4`}>
+          <span>
+            Case Study {count[0]} of {count[1]}:
+          </span>{" "}
+          {project.title}
+        </h4>
+      </div>
+
+      <p className="text-lg md:text-left">{project?.summary}</p>
+      <div className="flex justify-center items-center space-x-2">
+        {project.technologies.map(skill => (
+          <Image
+            src={urlFor(skill.image).url()}
+            alt={skill.title}
+            width={40}
+            height={40}
+            className="w-10 h-auto"
+            key={skill._id}
+          />
+        ))}
+      </div>
     </div>
-  </div>
+  );
 }
