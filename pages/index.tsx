1ece43d38bbb9b0a1cff27dc8645d5f2af779c6c
Mindaugas Straksys <gamerx14x@gmail.com>
2025-02-06 15:03:41 +0200

    SEO: Site title & description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import { Header, Hero, About, WorkExperience, Skills, Projects, Contact }
-  from '../components/index';
-import { useState } from 'react';
-import { Experience, PageInfo, Project, Skill, Social } from '../typings';
-import { fetchPageInfo, fetchExperiences, fetchSkills, fetchProjects, fetchSocials }
-  from '../utils/fetchData';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { Header, Hero, About, WorkExperience, Skills, Projects, Contact } from "../components/index";
+import { useState } from "react";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchPageInfo, fetchExperiences, fetchSkills, fetchProjects, fetchSocials } from "../utils/fetchData";
 
 type Props = {
   pageInfo: PageInfo;
@@ -14,56 +12,62 @@ type Props = {
   skills: Skill[];
   projects: Project[];
   socials: Social[];
-}
+};
 
 export default function Home({ pageInfo, experiences, skills, projects, socials }: Props) {
   //------------Dark/Light mode state-------------
   const [dark, setDark] = useState<boolean>(true);
 
   return (
-    <div className={`${!dark && 'light'} layout scrollbar
+    <div
+      className={`${!dark && "light"} layout scrollbar
      h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0`}>
       <Head>
-        <title>IP:PORT --folio</title>
-        <meta name="description" content="Nextjs portfolio" />
+        <title>IP:PORT --folio | eyyMinda</title>
+        <meta
+          name="description"
+          content="Portfolio made using NextJS, Typescript, Tailwind & Sanity.io with CMS | eyyMinda"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header dark={dark} setDark={setDark} socials={socials} />
 
-      <section id="hero" className='snap-start'>
+      <section id="hero" className="snap-start">
         <Hero dark={dark} pageInfo={pageInfo} isExperiences={experiences.length} />
       </section>
 
-      <section id="about" className='snap-center'>
+      <section id="about" className="snap-center">
         <About dark={dark} pageInfo={pageInfo} />
       </section>
 
-      {experiences.length && <section id="experience" className='snap-center'>
-        <WorkExperience dark={dark} experiences={experiences} />
-      </section>}
+      {experiences.length && (
+        <section id="experience" className="snap-center">
+          <WorkExperience dark={dark} experiences={experiences} />
+        </section>
+      )}
 
-      <section id="skills" className='snap-start'>
+      <section id="skills" className="snap-start">
         <Skills dark={dark} skills={skills} />
       </section>
 
-      <section id="projects" className='snap-start'>
+      <section id="projects" className="snap-start">
         <Projects dark={dark} projects={projects} />
       </section>
 
-      <section id="contact" className='snap-start'>
+      <section id="contact" className="snap-start">
         <Contact dark={dark} pageInfo={pageInfo} />
       </section>
 
-      <Link href='#hero'>
-        <footer className={`${!dark && 'light '}back-to-top`}>
+      <Link href="#hero">
+        <footer className={`${!dark && "light "}back-to-top`}>
           <span></span>
           <span></span>
           <p>back to top</p>
         </footer>
       </Link>
     </div>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
@@ -79,8 +83,8 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       experiences,
       skills,
       projects,
-      socials,
+      socials
     },
-    revalidate: 60 * 5,
-  }
+    revalidate: 60 * 5
+  };
 };
