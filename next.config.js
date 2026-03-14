dc04fb113490233ceeddd91c28f0fbe8bc95f879
Mindaugas Straksys <gamerx14x@gmail.com>
2026-03-15 00:59:32 +0200

    chore: next config, tsconfig, type decl, gitignore

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['cdn.sanity.io']
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.sanity.io', pathname: '/**' }
+    ]
   }
 }
 
